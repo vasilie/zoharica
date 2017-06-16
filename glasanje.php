425e517260ddf122a1ac20400784bf4f50cfda46
vasilie <vasilie.91@gmail.com>
2017-06-16 22:22:11 +0200

    glasanje fix

--- glasanje.php
+++ glasanje.php
@@ -8,21 +8,21 @@
 </head>
 <body onload="onload(), sirina(), startTime(), startDate()">
 <script type="text/javascript">
-			
+
 			var kliknuto=0
 			function izaberi(){
-			
+
 				if (kliknuto != 1) {
 
-					
+
 					pomeri();
 					kliknuto++;
-				
+
 				} else {
 					smanji();
 					vrati();
 					kliknuto=0;
-				}	
+				}
 			}
 			function startTime() {
 			    var today=new Date();
@@ -67,26 +67,26 @@
 				var b = document.getElementById('kl');
 				b.style.transform="rotate(0deg)";
 				b.style.WebkitTransform = "rotate(0deg)";
-				b.style.transition="all 2s"; 
+				b.style.transition="all 2s";
 			}
 			function rasti(){
-				
+
 				var h = document.getElementById('banner');
 				h.style.transition="all 2s";
 				h.style.background="blue";
 				h.style.width="900px";
-						
+
 			}
 			function smanji(){
-				
+
 				var h = document.getElementById('banner');
 				h.style.width="944px";
-				 
-			
+
+
 			}
 			function sirina(){
 				var x = window.innerWidth;
-				
+
 				var sirina2 =(x-230)/2;
 				var z =document.getElementById('dialogbox');
 				z.style.left=sirina2+"px";
@@ -105,7 +105,7 @@
 				b.style.display="none";
 			}
 			var nesicainfo="Nešica, žohar kakvog nema.";
-			
+
 			function selektor(ime){
 
 				document.getElementById("krpelj").innerHTML="Bravo, izabran je <span class='boja'>"+ime+"</span>.";
@@ -116,12 +116,12 @@
 
 			}
 
-			
+
 
 			function nista(){
 				document.getElementById("uvod").innerHTML="Dobrodosli na svecano krunisanje";
 				document.getElementById("razrada").innerHTML="Konačno imamo priliku da izaberemo predstavnika Balkanske škole žoharenja.  ";
-				
+
 			}
 			function smooth(){
 				document.getElementById('uvod').style.color="white";
@@ -136,44 +136,44 @@
 				document.getElementById("uvod").innerHTML=nesicainfo;
 				document.getElementById("razrada").innerHTML="Nešica, retko ko se moze pohvaliti ovolikim smislom za žoharenje, služi se tehnikom \"Žohari bližnjeg svog\". Mnoge generacije se služe njegovim stilom, čiji je najbitniji element nevini izgled. <br />Code name: Bogomoljka";
 				 smooth();
-				
+
 			}
-			
+
 			function dzoni(){
 				document.getElementById("uvod").innerHTML="Džoni, žohari od frenda.";
 				document.getElementById("razrada").innerHTML="Džoni pripada klasičnom tipu uličnog žohara, najbolji je u prikrivanju žoharenja, koristi se drevnom tehnikom \"Je l' mai štone za brata?\" naravno posle čega sledi \"A brat ne zna da \". Poznat je po lakom prihvatanju uličnog slenga pa u njegovom prisustvu možete čuti \"A de ga je brat\" i izraze tipa \"lagano\",\"slabije\", \"stopeza\"... <br />Code name: Ledji";
-				
+
 				 smooth();
 			}
 
 			function este(){
 				document.getElementById("uvod").innerHTML="Esteban, Sremska Kamenica.";
 				document.getElementById("razrada").innerHTML="Žohar sa druge strane zakona. Glavna odlika su mu veliki bicepsi, kojima privlači nevine žrtve obično ženskog pola. Nije poznata metoda žoharenja jer koristi tajne tehnike Ministarstva Unutrašnjih Poslova. Poznat po svom alter egu \"Estebanos\", brkatom Meksikancu koji pleni slabiji pol i isti poziva na blud. <br />Code name: 1312 ";
-				
+
 				smooth();
 			}
 			function manu(){
 				document.getElementById("uvod").innerHTML="Manu, večiti romantik.";
 				document.getElementById("razrada").innerHTML="Nagruvani vajar bicepsa, mašina za teg i prženje kalorija, pametnom odlukom da ne bude žohar prema svom telu dobija velike poene kod neukih seljančica iz Markovca i Velike Plane, koje nemilosrdno iskorišćava da bi povećao listu istih. Većina ga posmatra kao takvog, ali svaka koja je uspela da prodre u njegovu dušu osetila je koliko emocionalan div može biti. <br /> Metoda žoharenja: \"Klasična žoharica\".<br /> Code name: Zuba ";
-				
+
 				smooth();
 			}
 			function joca(){
 				document.getElementById("uvod").innerHTML="Joca, žohar iz spoljašnjosti.";
 				document.getElementById("razrada").innerHTML="Koristeći svoje gipko telo ovaj žohar starog kova uspeva da se ušunja u samu srž žoharenja, i time osigura svoje mesto kao lider ovog pokreta. Prošao je sve ulice i pročitao sve knjige, i stekao dovoljno iskustva da mu žoharanje postane rutina. Noću dobija +20 shadow skill i tad je nezaustavljiv.<br/ >Metoda žoharenja: \"Ivanova majka\".<br /> Code name: Žoha.";
-				
+
 				smooth();
 			}
 			function adam(){
 				document.getElementById("uvod").innerHTML="Adam, DMT šaman.";
 				document.getElementById("razrada").innerHTML="Ovo je žohar višeg reda, posvetio svoj život izučavanju mozga u interakciji sa prirodom. Često ispušta čudne krike kojima privlači pažnju u sezoni parenja. Odrekao se konformizma i otišao na brdo da živi sa biljkama.<br /> Metoda žoharenja: \"Stepski vuk\".<br /> Code name: \"Jelenko\" ";
-				
+
 				smooth();
 			}
 			function vasilie(){
 				document.getElementById("uvod").innerHTML="Vasilie, stara garda.";
 				document.getElementById("razrada").innerHTML="Iskrivljena figura je prvo što se primeti na njemu. Njega mrzi da jede, da spava, da postoji. Obično se oglašava rečenicom \"Majmune, koja šema?\", iako flegmatičan poseduje izuzetan njuh za žoharenje.<br /> Metoda žoharenja: \"Klasična žoharica\".<br /> Code name: Gara ";
-				
+
 				smooth();
 			}
 
@@ -183,100 +183,112 @@
 	<p id="suncanica">Pazi sta radiš, žoharice!</p>
 	<input type='submit' value="Ok" onClick="gasi()">
 	</div>
-<div id="content">		
+<div id="content">
 	<div id="nav"><ul>
 		<li><a href="index.php"><b>HOME</b></a></li>
 		<li><a href="kako.php"><b>ISTORIJA ŽOHARICE</b></a></li>
 		<li><a href="glasanje.php"><b>GLASANJE</b></a></li>
-		<li><a href="oblaci.html"><b>ABOUT</b></a></li>	
+		<li><a href="oblaci.html"><b>ABOUT</b></a></li>
 		<span id="smaracica">
-			<?php 
-				
+			<?php
+
 				if (isset($_COOKIE['username'])) {
 					echo "Dobrodošao " .$_COOKIE['username'];
 
 				} else {
 					echo "Dobrodošao neznanče. Skokni do <a href='index.php'>pocetne</a> na prijavicu laganu.";
 				}
-			 ?>		
-		</span>	
+			 ?>
+		</span>
 	</ul></div>
 
-	<div id="glavno">				
-		<div id="header"><h1>Žoharica</h1></div>		
-		<div id="banner">Glasanje</div>	
+	<div id="glavno">
+		<div id="header"><h1>Žoharica</h1></div>
+		<div id="banner">Glasanje</div>
 		<div id="glasanje">
 			<p class='nglasanja' onMouseOver="nista()">Ko je najveci žohar?</p>
 			<ul>
 				<form action="glasanje.php" method="POST" value="Glasaj" name="glasanje" >
-					<li class="imena">  
-						<label for="1" onMouseOver="nesica()" onClick="selektor('Nešica')">Nešica		</label>  
+					<li class="imena">
+						<label for="1" onMouseOver="nesica()" onClick="selektor('Nešica')">Nešica		</label>
 						<input id="1" type="radio" name="glasanje" value="nesica" onClick="selektor('Nešica')">
 					</li>
-					
+
 					<li class="imena">
 						<label for="2" onMouseOver="este()" onClick="selektor('Esteban')">Esteban		</label>
 						<input id="2" type="radio" name="glasanje" value="esteban" onClick="selektor('Esteban')">
-					</li> 
+					</li>
 
 					<li class="imena">
 						<label for="3" onMouseOver="manu()" onClick="selektor('Manu')">Manu			</label>
 						<input id="3" type="radio" name="glasanje" value="manu" onClick="selektor('Manu')">
-					</li>   
+					</li>
 
 					<li class="imena">
 						<label for="4" onMouseOver="dzoni()" onclick=" selektor('Džoni')">Džoni			</label>
 						<input id="4" type="radio" name="glasanje" value="dzoni" onClick="selektor('Džoni')">
-					</li>   
+					</li>
 
 					<li class="imena">
 						<label for="5"onMouseOver="adam()" onClick="selektor('Adam')">Adam			</label>
 						<input id="5" type="radio" name="glasanje" value="adam" onClick="selektor('Adam')">
-					</li>   
+					</li>
 
 					<li class="imena">
-						<label for="6" onMouseOver="vasilie()" onClick=" pali(), selektor('Vasilie')  ">Vasilie		</label>	
+						<label for="6" onMouseOver="vasilie()" onClick=" pali(), selektor('Vasilie')  ">Vasilie		</label>
 						<input id="6" type="radio" name="glasanje" value="vasilie" onClick="selektor('Vasilie')">
-					</li>   
+					</li>
 
 					<li class="imena" onMouseOver="joca()" >
 						<label for="7" onClick="selektor('Joca Žohar')">Joca Žohar		</label>
 						<input id="7" type="radio" name="glasanje" value="joca" onClick="selektor('Joca Žohar')">
-					</li>   	
+					</li>
 			</ul>
 					<input value="Glasaj" class="zoha" type="submit" >
 				</form>
-			
-				
+
+
 
 
 
 		</div>
-			
+
 
 		<div id="zezanje";>
-			<div id="status">	
-			<?php 
+			<div id="status">
+			<?php
 			$pozdrav="<p style='welcome'>A ovde su rezultati</p>";
-			
-		//-------------- Konekcija SQL, uzima informacije iz smradovi, i ubacuje u  promenljive--------------------
+
+		//-------------- Konekcija SQL, uzima informacije iz zohari, i ubacuje u  promenljive--------------------		$servername = "localhost";
+		$username = "root";
+		$password = "";
+
+		// Create connection
+		$db = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($db->connect_error) {
+		    die("Connection failed: " . $db->connect_error);
+		}
+		echo "Connected successfully";
 
 
-					$db = mysql_connect('localhost' , 'zoharica', 'winstonblue' , zoharica_app) or die("Doslo je do problema");
-					
-					mysql_select_db(zoharica_app,$db);
-					
-					$sql = "SELECT * FROM smradovi";
-					
-					$result = mysql_query($sql, $db);
-					
-			while ($row = mysql_fetch_array($result)) {
-						  
+					// $db = mysql_connect('localhost' , 'zoharica', 'winstonblue' , "zoharica");
+
+					 mysqli_select_db( $db,'zoharica');
+
+					$sql = "SELECT * FROM zohari";
+					$result = mysqli_query($db, $sql);
+					//var_dump($result);
+
+
+			while ($row = mysqli_fetch_array($result)) {
+
 
 					if ($row['ime']=="Nesica")	{
 							$nesica=$row['poeni'];
 							;
-							
+
 					}
 
 
@@ -307,32 +319,32 @@
 		//-------------------------------- Do ovde--------------------------------------------------------------------
 		//------------------------------------------------------------------------------------------------------------
 
-			mysql_close($db);
+
 		// Ako ip adresa postoji u fajlu 'adresa.txt' $glasanje je false
 				$txt="kara.html";
 				$br=0;
 				$glasanje=true;
 				$zezanje=file_get_contents('kara.html');
-				
+
 				$adresa=$_SERVER['REMOTE_ADDR'];
 				$lines = explode("\n", $zezanje);
 				$adresa1 = (string) $adresa;
-				foreach ($lines as $line) { 
+				foreach ($lines as $line) {
 
 					if ($line==$adresa1){
-						
-						$glasanje=false; 
-						
-					}					
-					
+
+						$glasanje=false;
+
+					}
+
 				}
 
-				
-
-				
 
 
-				
+
+
+
+
 
 
 
@@ -347,10 +359,11 @@
 				}
 
 				*/
-				$db = mysql_connect('localhost' , 'zoharica', 'winstonblue' , zoharica_app) or die("Doslo je do problema");
-					
-				mysql_select_db(zoharica_app,$db);
-				$zohari=$_POST{"glasanje"};
+
+
+				mysqli_select_db( $db,'zoharica');
+if (isset($_POST{"glasanje"})) {	$zohari=$_POST{"glasanje"};
+}
 				if (isset($zohari)) {
 					# code...
 					echo "
@@ -359,7 +372,7 @@
 								.welcome {
 
 								display: none;
-								
+
 
 								}
 
@@ -367,102 +380,102 @@
 							</style>
 						";
 
-					if ($glasanje==true) { 
-					
-				
+					if ($glasanje==true) {
 
 
-						
+
+
+
 						switch ($zohari)
 							{
 								case "nesica":
-									
+
 											$nesica++;
-									  		mysql_query("UPDATE smradovi SET poeni='$nesica' WHERE id='1'");
+									  		mysqli_query($db,"UPDATE zohari SET poeni='$nesica' WHERE id='1'");
 									  		$lik="Nešicu";
 									break;
 
-								
+
 
 
 								case "dzoni":
-								
+
 									  		$dzoni++;
-									  		mysql_query("UPDATE smradovi SET poeni='$dzoni' WHERE id='4'");
+									  		mysqli_query($db,"UPDATE zohari SET poeni='$dzoni' WHERE id='2'");
 									  		$lik="Džonija";
 									break;
-								
+
 
 								case "vasilie":
-									
+
 									  		$vasilie++;
-									  		mysql_query("UPDATE smradovi SET poeni='$vasilie' WHERE id='3'");
+									  		mysqli_query($db,"UPDATE zohari SET poeni='$vasilie' WHERE id='3'");
 									  		$lik="Vasilia";
-									break;	  
-								
+									break;
+
 			  					case "joca":
-									
+
 									  		$joca++;
-									  		mysql_query("UPDATE smradovi SET poeni='$joca' WHERE id='2'");
+									  		mysqli_query($db,"UPDATE zohari SET poeni='$joca' WHERE id='6'");
 									  		$lik="Jocu";
 									break;
 
-								
+
 								case "manu":
-								
+
 								  			$manu++;
-								  			mysql_query("UPDATE smradovi SET poeni='$manu' WHERE id='5'");
+								  			mysqli_query($db,"UPDATE zohari SET poeni='$manu' WHERE id='4'");
 								  			$lik="Manua";
 								break;
-								
+
 								case "esteban":
-								
+
 								  			$esteban++;
-								  			mysql_query("UPDATE smradovi SET poeni='$esteban' WHERE id='6'");
+								  			mysqli_query($db,"UPDATE zohari SET poeni='$esteban' WHERE id='7'");
 								  			$lik="Estebana";
 								break;
-								
+
 								case "adam":
-								
+
 								  			$adam++;
-								  			mysql_query("UPDATE smradovi SET poeni='$adam' WHERE id='7'");
+								  			mysqli_query($db,"UPDATE zohari SET poeni='$adam' WHERE id='5'");
 								  			$lik="Adama";
 								break;
-							
-								
-								
+
+
+
 
 							}
 							$pozdrav = "Glasali ste za " . $lik;
 							$txt="kara.txt";
 							$adresa=$_SERVER['REMOTE_ADDR'];
 							file_put_contents('kara.html', $adresa . PHP_EOL, FILE_APPEND);
-							$poslednji=$_COOKIE['username'];
-							$vremeglasanja=time("H:i:s");
+
+							$vremeglasanja=time();
 							$vr = "$vremeglasanja";
-							if (isset($_COOKIE['username'])){
-								mysql_query("UPDATE vremeg SET vreme='$vr' WHERE id='1'");
-								mysql_query("UPDATE glasao SET ime='$poslednji' WHERE id='1'");
+							if (isset($_COOKIE['username'])){								$poslednji=$_COOKIE['username'];
+								mysqli_query($db,"UPDATE vremeg SET vreme='$vr' WHERE id='1'");
+								mysqli_query($db,"UPDATE glasao SET ime='$poslednji' WHERE id='1'");
 							}
 
 					} else {
 					$pozdrav = "Mozes glasati jednom dnevno";
 					}
-				
-					
-					
-				}	
-				
+
+
+
+				}
+
 
 				echo $pozdrav;
 				$vreme=date('H');
-				$result1 = mysql_query("SELECT * FROM vreme WHERE id=1 LIMIT 1");
-			
-				$row = mysql_fetch_assoc($result1);
+				$result1 = mysqli_query($db, "SELECT * FROM vreme WHERE id=1 LIMIT 1");
+				// var_dump($result1);
+				$row = mysqli_fetch_assoc($result1);
 				$vremesad = time();
-				$vzreset = $row['vreme']; // Vreme zadnjeg resetovanja	
-				
-				if ($vremesad > $vzreset + 24 * 60 * 60){	
+				$vzreset = $row['vreme']; // Vreme zadnjeg resetovanja
+
+				if ($vremesad > $vzreset + 24 * 60 * 60){
 					$brisi = true;
 					;
 				} else {
@@ -471,7 +484,7 @@
 					//echo "brisi = false";
 				}
 
-				if($brisi==true ){ 
+				if($brisi==true ){
 
 					unlink($txt);
 					// ovde kod za pravljenje novog fajla
@@ -479,22 +492,22 @@
 					$ourFileHandle = fopen($ourFileName, 'w') or die("can't open file");
 					fclose($ourFileHandle);
 					$vreset = time(); // vreme resetovanja
-					mysql_query("UPDATE vreme SET vreme='$vreset' WHERE ID=1");
-					
-				}	
+					mysqli_query($db,"UPDATE vreme SET vreme='$vreset' WHERE id=1");
 
-				
-				$ukupno = $nesica + $adam + $manu + $dzoni + $esteban + $joca + $vasilie; 
-	  
+				}
+
+
+				$ukupno = $nesica + $adam + $manu + $dzoni + $esteban + $joca + $vasilie;
+
 
 
 
 
 	 			?>
-			
 
-			</div>	
-			
+
+			</div>
+
 			<div id="rezultati">
 				<div id="rezl">
 				<?php
@@ -512,8 +525,8 @@
 							<li>Esteban: </li>
 							<li>Manu: </li>
 							<li>Džoni: </li>
-							<li>Adam: </li>	
-							<li>Vasilie: </li>	
+							<li>Adam: </li>
+							<li>Vasilie: </li>
 							<li>Joca Žohar: </li>
 						</ul>
 
@@ -553,7 +566,7 @@
 				?>
 				</div>
 				<?php
-					echo "<p class='ukup'>Ukupno glasova: $ukupno <span class='vote'>Votes</span></p>" 
+					echo "<p class='ukup'>Ukupno glasova: $ukupno <span class='vote'>Votes</span></p>"
 				?>
 			</div>
 			<div id="selektor">
@@ -565,36 +578,35 @@
 					<p id="vreml">Vreme: </p>
 					<p id="datuml">Datum: </p>
 				</div>
-				
+
 				<div id="vdesno">
 					<p id="vrem"></p>
 					<p id="datum"></p>
 				</div>
-				
+
 			</div>
 		</div>
-		<div id="info">	
+		<div id="info">
 			<div class="zeka">
 			<p class="nglasanja1">Dobrodosli na svecano krunisanje</p>
 			<p class="nakaza">Konačno imamo priliku da izaberemo predstavnika Balkanske škole žoharenja.</p>
 			</div>
 				<p class="nglasanja3" id="uvod"></p>
 				<p id="razrada" class="pinfo"></p>
-			
+
 		</div>
 		<div id="glasao">
-			<p style="margin-bottom:4px;">Poslednji je glasao:<span class="boja"> 
-				<?php 
+			<p style="margin-bottom:4px;">Poslednji je glasao:<span class="boja">
+				<?php
 
-					$result2 = mysql_query("SELECT * FROM glasao WHERE id=1 LIMIT 1");
-					$row2 = mysql_fetch_assoc($result2);
+					$result2 = mysqli_query($db,"SELECT * FROM glasao WHERE id=1 LIMIT 1");					$row2 = mysqli_fetch_assoc($result2);
 					$SQLposlednji = $row2['ime'];
-					$result3 = mysql_query("SELECT * FROM vremeg WHERE id=1 LIMIT 1");
-					$row3 = mysql_fetch_assoc($result3);
+					$result3 = mysqli_query($db,"SELECT * FROM vremeg WHERE id=1 LIMIT 1");
+					$row3 = mysqli_fetch_assoc($result3);
 					$vremeg = $row3['vreme'];
-					mysql_close($db);
-					echo $SQLposlednji; 
-					
+					mysqli_close($db);
+					echo $SQLposlednji;
+
 				?>
 				</span>
 			</p>
@@ -603,16 +615,14 @@
 
 
 
-		
 
-		
+
+
 	</div>
 	<div id="footer">copyright &copy; 2014 Vasilije Milenković</div>
-	</div>		
-		
+	</div>
+
 </div>
 
 </body>
 </html>
-
-
---
+++ zoharica.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2017 at 10:21 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zoharica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `glasao`
+--
+
+CREATE TABLE IF NOT EXISTS `glasao` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `glasao`
+--
+
+INSERT INTO `glasao` (`id`, `ime`) VALUES
+(0, ''),
+(0, ''),
+(1, 'Omladin'),
+(1, 'Omladin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vreme`
+--
+
+CREATE TABLE IF NOT EXISTS `vreme` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vreme` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `vreme`
+--
+
+INSERT INTO `vreme` (`id`, `vreme`) VALUES
+(1, '0000-00-00 00:00:00'),
+(2, '0000-00-00 00:00:00'),
+(3, '0000-00-00 00:00:00'),
+(4, '2017-06-16 19:14:56'),
+(5, '2017-06-16 19:14:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vremeg`
+--
+
+CREATE TABLE IF NOT EXISTS `vremeg` (
+  `id` int(11) NOT NULL,
+  `vreme` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vremeg`
+--
+
+INSERT INTO `vremeg` (`id`, `vreme`) VALUES
+(1, '838:59:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zohari`
+--
+
+CREATE TABLE IF NOT EXISTS `zohari` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ime` varchar(32) NOT NULL,
+  `poeni` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `zohari`
+--
+
+INSERT INTO `zohari` (`id`, `ime`, `poeni`) VALUES
+(1, 'Nesica', 1),
+(2, 'Dzoni', 8),
+(3, 'Vasilie', 3),
+(4, 'Manu', 8),
+(5, 'Adam', 10),
+(6, 'Joca', 4),
+(7, 'Esteban', 4);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
